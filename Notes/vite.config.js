5a7821db1661f45f7ae816e313f5d110c9542d64
pujeetas <pujeeta98@gmail.com>
2025-12-29 22:08:36 +0530

    minor changes

--- Notes/vite.config.js
+++ Notes/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.BACKEND_URL,
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
