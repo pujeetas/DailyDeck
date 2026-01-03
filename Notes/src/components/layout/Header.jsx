58d72a981c240cd8c37fc9df07e48a647e743a34
pujeetas <pujeeta98@gmail.com>
2026-01-03 08:45:19 +0530

    minor update

--- Notes/src/components/layout/Header.jsx
+++ Notes/src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ import { HomeFilled } from "@ant-design/icons";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useUserStore from "../../hooks/useUserStore";
-import UserDropdownMenu from "@/constants/userDropdownMenu.jsx";
+import UserDropdownMenu from "@/constants/UserDropdownMenu";
 
 const Header = ({ color }) => {
   const navigate = useNavigate();
